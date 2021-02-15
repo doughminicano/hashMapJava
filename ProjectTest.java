8ec151b2eb5cf6b57297e4a8b5d6f29812b75690
doughminicano <dg22lg@gmail.com>
2021-02-15 16:00:23 -0500

    first commit

---
+++ ProjectTest.java
@@ -0,0 +1,14 @@
+public class ProjectTest {
+    public static void main(String[] args) {
+
+
+Project elavatorPitch = new Project("Dan", "The Man!!!!!.... Chillllllld!!!!!!!");
+
+System.out.println(elavatorPitch.getName() +":" + elavatorPitch.getDescription());
+
+Project diegoC = new Project("Diego", "SpecOps_Mission: *************");
+
+System.out.println(diegoC.getName() +":" + diegoC.getDescription());
+
+    }
+}
